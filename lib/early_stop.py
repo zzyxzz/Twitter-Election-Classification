0647331fcdedde22b06066d6f4bd3f3009691bb4
Xiao Yang <yxzz@hotmail.com>
2019-09-17 21:38:23 +0100

    update twitter api info

---
+++ lib/early_stop.py
@@ -0,0 +1,25 @@
+class EarlyStopping(object):
+    def __init__(self, threshold=5):
+        self.threshod = threshold
+        self.scores = []
+        # performance decrease
+        self.consecutive_decrease = 0
+
+    def stop_training(self, score, step):
+        '''
+        :param score: score on validation set
+        :param step: iteration step
+        :return: boolean
+        '''
+        self.scores.append(score)
+
+        if len(self.scores) > 1 and self.scores[-1] > self.scores[-2]:
+            self.consecutive_decrease += 1
+        else:
+            self.consecutive_decrease = 0
+        # print self.consecutive_decrease, self.scores
+        if self.consecutive_decrease >= self.threshod:
+            print("Early stopping criteria {} reached! Stopped at step: {}".format(self.threshod, step))
+            return True
+        else:
+            return False
